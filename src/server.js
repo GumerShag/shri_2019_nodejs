d1eb9364e78d9df78117e227085d8d93d69ecc1f
Gumer Shagiyanov <>
2019-09-14 02:06:40 +0300

    Provide all required end-points

--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const os = require('os');
 const {exec} = require('child_process');
+const bodyParser = require('body-parser');
 const args = require('minimist')(process.argv.slice(2));
 const app = express();
 const repoPath = args.p;
@@ -12,89 +14,126 @@ app.get('/api/repos/', (req, res) => {
         res.json({error: 'Wrong path to repositories folder.'});
         return;
     }
-    fs.readdir(absoluteRepoPath, (err, files) => {
+    fs.readdir(absoluteRepoPath, (err, repos) => {
         if (err) {
             console.log(err);
             return;
         }
-        res.set('Content-Type', 'application/json').send(JSON.stringify(files));
+        res.set('Content-Type', 'application/json').send(repos.map(id => ({id})));
     })
 
 });
 
 app.get('/api/repos/:repositoryId/commits/:commitHash', (req, res) => {
-   const {repositoryId} = req.params;
-   const {commitHash} = req.params;
-
-    if (!commitHash) {
-        exec(`git log`, {cwd: `${absoluteRepoPath}/${repositoryId}`}, (err, stdout, stderr) => {
-            if (err) {
-                console.log(err);
+    const {repositoryId} = req.params;
+    const {commitHash} = req.params;
+    exec(`git rev-parse ${commitHash}`, {cwd: `${absoluteRepoPath}/${repositoryId}`}, (err, stdout) => {
+        if (err) {
+            if (err.message.indexOf('unknown revision') > -1) {
+                res.status(404).json({error: `404. ${commitHash}: no such commit or branch`});
                 return;
             }
-            res.set('Content-Type', 'application/json').send(JSON.stringify(stdout));
-        })
-    } else {
-        exec(`git log ${commitHash}`, {cwd: `${absoluteRepoPath}/${repositoryId}`}, (err, stdout, stderr) => {
-            if (err) {
-                if(err.message.indexOf('unknown revision') > -1) {
-                    res.status(404).json({error: '404. No such commit'});
+            return;
+        }
+        const branchHash = stdout.trim();
+        exec('git --no-pager log ' + branchHash + ' --pretty=format:"{@commitHash@: @"%H"@,@message@: @"%s"@,@date@: @"%cd"@}"',
+            {cwd: `${absoluteRepoPath}/${repositoryId}`},
+            (err, logData) => {
+                if (err) {
+                    console.error(`exec error: ${err}`);
                     return;
                 }
-                return;
-            }
-
-            res.set('Content-Type', 'application/json').send(JSON.stringify(stdout));
-        })
-    }
+                let commitsArray = logData.replace(/@/g, '"').split('\n');
+                res.json(
+                    commitsArray.map((commit) => {
+                        return JSON.parse(`${commit}`)
+                    })
+                );
+            });
+    });
 });
 
 app.get('/api/repos/:repositoryId/commits/:commitHash/diff', (req, res) => {
     const {repositoryId} = req.params;
     const {commitHash} = req.params;
 
-        exec(`git diff ${commitHash}~ ${commitHash}`, {cwd: `${absoluteRepoPath}/${repositoryId}`}, (err, stdout, stderr) => {
-            if (err) {
-                console.log(err);
-                return;
-            }
-            res.set('Content-Type', 'application/json').send(JSON.stringify(stdout));
-        })
+    exec(`git diff ${commitHash}~ ${commitHash}`, {cwd: `${absoluteRepoPath}/${repositoryId}`}, (err, stdout) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        res.json({diff: stdout});
+    })
 
 });
 
-app.get(['/api/repos/:repositoryId', '/api/repos/:repositoryId/tree/:commitHash?/:path?'], (req, res) => {
+app.get(['/api/repos/:repositoryId', '/api/repos/:repositoryId/tree/:commitHash?/:path*'], (req, res) => {
     const {repositoryId} = req.params;
     const {commitHash} = req.params;
     const {path} = req.params;
+
+    //todo: hack
     if (req.originalUrl.indexOf('tree') > -1) {
-        exec(`git rev-parse ${commitHash}`, {cwd: `${absoluteRepoPath}/${repositoryId}`}, (err, stdout, stderr) => {
+        exec(`git rev-parse ${commitHash}`, {cwd: `${absoluteRepoPath}/${repositoryId}`}, (err, commitHash) => {
             if (err) {
-                console.log(err);
+                res.status(500).json({error: err});
                 return;
             }
-            const branchHash = stdout;
-            exec(`git ls-tree --name-only ${commitHash} ${path}`, {cwd: `${absoluteRepoPath}/${repositoryId}`}, (err, stdout, stderr) => {
+            const branchHash = commitHash;
+            exec(`git ls-tree --name-only ${branchHash} ${path}`, {cwd: `${absoluteRepoPath}/${repositoryId}`}, (err, content) => {
                 if (err) {
                     console.log(err);
                     return;
                 }
-                res.set('Content-Type', 'application/json').send(JSON.stringify(stdout));
+
+                const contentList = content.split('\n');
+                res.json(contentList.map(name => name));
 
             });
 
         });
-        res.send('tree')
     } else {
         //fixme: Should return from Main branch
-        exec(`git ls-tree HEAD --name-only`, {cwd: `${absoluteRepoPath}/${repositoryId}`}, (err, stdout, stderr) => {
+        exec(`git ls-tree HEAD --name-only`, {cwd: `${absoluteRepoPath}/${repositoryId}`}, (err, content) => {
             if (err) {
                 console.log(err);
                 return;
             }
-            res.set('Content-Type', 'application/json').send(JSON.stringify(stdout));
+            const contentList = content.split('\n');
+            res.json(contentList.map(name => name));
         })
     }
 });
-app.listen(3000);
 
+app.delete('/api/repos/:repositoryId', (req, res) => {
+    const {repositoryId} = req.params;
+    const command = os.platform === 'win32' ? 'rmdir' : 'rm -rf';
+
+    exec(`${command} ${repositoryId}`, {cwd: `${absoluteRepoPath}`}, (err) => {
+        if (err) {
+            res.status(500).json({message: err});
+            return;
+        }
+        res.json({message: 'OK'});
+    })
+});
+
+app.post('/api/repos/:repositoryId', bodyParser.urlencoded(), (req, res) => {
+    const {repositoryId} = req.params;
+    const {url} = req.body;
+
+    exec(`git clone ${url} ${repositoryId}`, {cwd: `${absoluteRepoPath}`}, (err) => {
+        if (err) {
+            if (err.message.indexOf('HttpRequestException encountered') > -1) {
+                res.status(404).json({message: "Repository doesn't exist"});
+                return;
+            }
+            res.status(500).json({message: err});
+            return;
+        }
+
+        res.status(201).json({message: 'OK', id: repositoryId});
+    })
+});
+
+app.listen(3000);
